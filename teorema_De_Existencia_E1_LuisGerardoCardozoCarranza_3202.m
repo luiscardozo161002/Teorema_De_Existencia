e9dbc78598a5ede39dab533def809ef3909ebddf
luiscardozo161002 <78442724+luiscardozo161002@users.noreply.github.com>
2021-05-04 17:38:27 -0500

    Update teorema_De_Existencia_E1_LuisGerardoCardozoCarranza_3202.m

--- teorema_De_Existencia_E1_LuisGerardoCardozoCarranza_3202.m
+++ teorema_De_Existencia_E1_LuisGerardoCardozoCarranza_3202.m
@@ -8,7 +8,6 @@
 % Notes			:Requiere aplicacion octave usar en consola preferentemente.
 
 %Actividad_Ejercicio 1 f(x)=e^x,[0,4]
-% Si se graficá su comportamiento es exponencial
 %Limpiar variables.
 clear
 %Dominio de la funcion.
