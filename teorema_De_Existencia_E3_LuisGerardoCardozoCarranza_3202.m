9f49c3218fe85cd969831e95980ac05b4231c1a1
luiscardozo161002 <78442724+luiscardozo161002@users.noreply.github.com>
2021-05-04 09:59:27 -0500

    Update teorema_De_Existencia_E3_LuisGerardoCardozoCarranza_3202.m

--- teorema_De_Existencia_E3_LuisGerardoCardozoCarranza_3202.m
+++ teorema_De_Existencia_E3_LuisGerardoCardozoCarranza_3202.m
@@ -1,24 +1,24 @@
 % Octave Script
 % Title			:1.5 Teorema de Existencia
-% Description		:Script para graficar el ejericio 3 de la actividad 1.5
+% Description		:Script para graficar el ejercicio 3 de la actividad 1.5
 % Author		:Luis Gerardo Cardozo Carranza (Gerard_CRS) luiscardozo161002@gmail.com
 % Date			:20210504
 % sion		        :1
 % Usage			:octave> /path/Teorema_de_Existencia_Actividad_1.5_Luis Gerardo Cardozo Carranza_3202
 % Notes			:Requiere aplicacion octave usar en consola preferentemente.
 
-%Actividad_Ejercicio 3 f(x)=ln(x),[1,4]
+%Actividad_Ejercicio 3 f(x)=1/(1+x^2),[-1,1]
 % Si se graficá su comportamiento es exponencial
 %Limpiar variables.
 clear
 %Dominio de la funcion.
-x=1:0.1:4;
+x=-1:0.1:1;
 %Valor de la funcion.
-y=(log(x));
+y=1./(1+x.^2);
 %Genera la grafica 
 plot(x,y,'b');
 %Asigna un nombre a la grafica 
-title("Ejercicio 3 f(x)=ln(x),[1,4]");
+title("Ejercicio 3 f(x)=1/(1+x^2),[-1,1]");
 %Etiqueta para x
 xlabel("x");
 %Etiqueta para y
