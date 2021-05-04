2e21099925155b7b55e8155b993757b1c79641ac
luiscardozo161002 <78442724+luiscardozo161002@users.noreply.github.com>
2021-05-04 09:12:55 -0500

    Create teorema_De_Existencia_E1_LuisGerardoCardozoCarranza_3202.m

---
+++ teorema_De_Existencia_E1_LuisGerardoCardozoCarranza_3202.m
@@ -0,0 +1,25 @@
+% Octave Script
+% Title			      :1.5 Teorema de Existencia
+% Description	    :Script para graficar el ejericio 1 de la actividad 1.5
+% Author		      :Luis Gerardo Cardozo Carranza (Gerard_CRS) luiscardozo161002@gmail.com
+% Date			      :20210504
+% sion		        :1
+% Usage		      	:octave> /path/Teorema_de_Existencia_Actividad_1.5_Luis Gerardo Cardozo Carranza_3202
+% Notes		      	:Requiere aplicacion octave usar en consola preferentemente.
+
+%Actividad_Ejercicio 1 f(x)=e^x,[0,4]
+% Si se graficá su comportamiento es exponencial
+%Limpiar variables.
+clear
+%Dominio de la funcion.
+x=0:0.1:4;;
+%Valor de la funcion.
+y=(e.^x);
+%Genera la grafica 
+plot(x,y,'b');
+%Asigna un nombre a la grafica 
+title("Ejericicio 1 f1(x)=e^x");
+%Etiqueta para x
+xlabel("x");
+%Etiqueta para y
+ylabel("y");
